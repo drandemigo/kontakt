d752f733d601227b2bab9bce831c90e170a542cf
drandemigo <drandemigo@gmail.com>
2018-03-06 14:47:54 +0200

    Initial commit

---
+++ src/Kontakt.java
@@ -0,0 +1,17 @@
+
+
+import lombok.*;
+@Getter @Setter
+@AllArgsConstructor @ToString
+@EqualsAndHashCode
+
+public class Kontakt {
+private int id;
+    private String name;
+    private String name;
+    private String name;
+
+
+
+
+}
